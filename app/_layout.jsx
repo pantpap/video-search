ce60ecf817447dbc524c6061e7f40d92c2eef3da
Pantelis Papasavvas <pantpap@gmail.com>
2024-04-24 09:40:42 +0300

    feat(general): render default page using Slot form expo router

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Slot } from 'expo-router'
+import React from "react";
 
-export default function App() {
+const RootLayout= () => {
     return (
-        <View style={styles.container}>
-            <Text>Aora!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+       <Slot/>
+    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export default RootLayout;
