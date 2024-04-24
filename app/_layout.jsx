24772bb91d944388562469f3c0f7393ef8f015f8
Pantelis Papasavvas <pantpap@gmail.com>
2024-04-24 10:10:51 +0300

    feat(general): navigation using Stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,13 @@
-import { Slot } from 'expo-router'
+import { Slot, Stack } from 'expo-router'
 import React from "react";
 
 const RootLayout= () => {
     return (
-       <Slot/>
+       <Stack>
+           <Stack.Screen name="index" options={{headerShown: false}}/>
+       </Stack>
+      
+      
     )
 }
 
