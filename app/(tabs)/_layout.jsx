f94f7e246595d1e92eec3f7d3419e33c26d98026
Pantelis Papasavvas <pantpap@gmail.com>
2024-04-25 10:04:52 +0300

    feat(tabs): create tab screen for Home

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,41 @@
-import {View, Text} from "react-native";
+import {View, Text, Image} from "react-native";
+import { Tabs } from 'expo-router';
+import { icons } from '../../constants';
+
+const TabIcon = ({icon, color, name, focused}) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className={`w-6 h-6 ${focused ? 't-primary' : 't-gray-500'}`}
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs screenOptions={{
+        tabBarShowLabel: false
+      }}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({color, focused}) => (
+              <TabIcon icon={icons.home} color={color} name='Home' focused={focused}/>
+            )
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
