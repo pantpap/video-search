65bc73f91235983d8b07cb68566129bb3023259c
Pantelis Papasavvas <pantpap@gmail.com>
2024-04-29 11:52:29 +0300

    feat(general): implement first page (index.jsx)

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import {Image, ScrollView, Text, View} from 'react-native';
 import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import { images } from '../constants';
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className='text-3xl font-pblack'>Nephos Clip FInder!</Text>
-            <StatusBar style="auto" />
-            <Link href="/home" style={{color: 'blue'}}>Go to Home page</Link>
-        </View>
+      <SafeAreaView className={"bg-primary h-full"}>
+        <ScrollView contentContainerStyle={{height: '100%'}}>
+          <View className="w-full items-center h-full px-4">
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode="contain"
+            />
+            <Image
+              source={images.cards}
+              className="max-w--[380px] w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="relative mt-5">
+              <Text className="text-3xl text-white font-bold text-center">
+                Discover Endless Possibilities with{' '}
+                <Text className="text-secondary-200">Nephos</Text>
+              </Text>
+              
+              <Image
+                source={images.path}
+                className="w-[170px] absolute -bottom-3 -right-8 h-[20px]"
+                resizeMode="contain"
+              />
+            </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+              Where creativity meets innovation:
+              embark on a journey of limitless exploration with Nephos.
+            </Text>
+            <CustomButton />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+        // <View className="flex-1 items-center justify-center bg-white">
+        //     <Text className='text-3xl font-pblack'>Nephos Clip FInder!</Text>
+        //     <StatusBar style="auto" />
+        //     <Link href="/home" style={{color: 'blue'}}>Go to Home page</Link>
+        // </View>
     );
 }
