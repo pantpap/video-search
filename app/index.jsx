f2b07e6c1997d33bb5aa417f354eda4619dd18ae
Pantelis Papasavvas <pantpap@gmail.com>
2024-04-29 12:08:42 +0300

    feat(general): redirect you to the sign-in page

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {Image, ScrollView, Text, View} from 'react-native';
-import {Link} from "expo-router";
+import {Redirect, router} from "expo-router";
 import {SafeAreaView} from "react-native-safe-area-context";
 import { images } from '../constants';
 import CustomButton from "../components/CustomButton";
@@ -38,7 +38,7 @@ export default function App() {
             </Text>
             <CustomButton
               title="Continue with Email"
-              handlePress={() => console.log('Button pressed')}
+              handlePress={() => router.push('/sign-in')}
               containerStyles="w-full mt-7"
             />
           </View>
