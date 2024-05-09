e62d711962bd95e863c904b83d6d9ecef96b1a41
Pantelis Papasavvas <pantpap@gmail.com>
2024-05-09 09:26:54 +0300

    feat(sing-in): start implementing login screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,18 @@
-import {View, Text} from "react-native";
+import {View, Text, ScrollView, Image} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+import {images} from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-white text-2xl text-semibold mt-10 font-psemibold">Log in to Nephos</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
